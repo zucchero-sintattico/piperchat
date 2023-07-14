1c980b26f305cede18cee5083b01a198cf365df4
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-07-14 16:22:11 +0200

    Removed token generation from other microservices

--- services/piperchat/src/utils/jwt.ts
+++ services/piperchat/src/utils/jwt.ts
@@ -15,22 +15,6 @@ declare global {
 	}
 }
 
-export const generateAccessToken = (user: any) => {
-	return jwt.sign(
-		{ username: user.username, email: user.email, id: user._id } as UserJWTInfo,
-		process.env.ACCESS_TOKEN_SECRET || "access",
-		{ expiresIn: "15s" }
-	);
-};
-
-export const generateRefreshToken = (user: any) => {
-	return jwt.sign(
-		{ username: user.username, email: user.email, id: user._id } as UserJWTInfo,
-		process.env.REFRESH_TOKEN_SECRET || "refresh",
-		{ expiresIn: "1d" }
-	);
-};
-
 export const jwtValidTokenRequired = (req: Request, res: any, next: any) => {
 	console.log("[JWT-Middleware] - Checking if JWT Token is valid");
 	const accessToken = req.cookies.jwt;
