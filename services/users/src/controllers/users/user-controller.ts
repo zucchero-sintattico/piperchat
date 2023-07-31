50f305fa4e64b674878e984eb24f621aa970a8d2
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-07-31 09:12:40 +0200

    Added throws in user controller interface doc

--- services/users/src/controllers/users/user-controller.ts
+++ services/users/src/controllers/users/user-controller.ts
@@ -21,18 +21,24 @@ export interface UserController {
 	 * @param username The username of the user.
 	 * @param password The password of the user.
 	 * @returns The access token.
+	 * @throws {InvalidUsernameOrPassword} If the username or password is invalid.
 	 */
 	login(username: string, password: string): Promise<string>;
 
 	/**
 	 * Logout a user.
 	 * @param username The username of the user.
+	 * @throws {UserNotFound} If the user is not found.
 	 */
 	logout(username: string): Promise<void>;
 
 	/**
 	 * Refresh the access token of a user.
 	 * @param username The username of the user.
+	 * @returns The new access token.
+	 * @throws {UserNotFound} If the user is not found.
+	 * @throws {RefreshTokenNotPresent} If the refresh token is not present.
+	 * @throws {InvalidRefreshToken} If the refresh token is invalid.
 	 */
 	refreshToken(username: string): Promise<string>;
 }
