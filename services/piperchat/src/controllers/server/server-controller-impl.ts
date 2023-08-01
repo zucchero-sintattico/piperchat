a80ba17672abe6c86754a37d57522c5e45d3f12b
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-08-01 12:48:23 +0200

    Added ServerController

---
+++ services/piperchat/src/controllers/server/server-controller-impl.ts
@@ -0,0 +1,27 @@
+import { Server } from "../../models/server-model";
+import { ServerController } from "./server-controller";
+
+export class ServerControllerImpl implements ServerController {
+	getServers(username: string): Promise<Server[]> {
+		throw new Error("Method not implemented.");
+	}
+	getServer(id: number): Promise<Server> {
+		throw new Error("Method not implemented.");
+	}
+	updateServer(
+		id: number,
+		name?: string | undefined,
+		description?: string | undefined
+	): Promise<Server> {
+		throw new Error("Method not implemented.");
+	}
+	deleteServer(id: number): Promise<Server> {
+		throw new Error("Method not implemented.");
+	}
+	joinServer(id: number): Promise<Server> {
+		throw new Error("Method not implemented.");
+	}
+	leaveServer(id: number): Promise<Server> {
+		throw new Error("Method not implemented.");
+	}
+}
