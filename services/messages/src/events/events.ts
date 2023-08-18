5b869c33941b4076a5160269862dc282677850e0
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-08-18 16:10:12 +0200

    fix: fixed tests setupping ServiceEvents and RabbitMQ in test files

--- services/messages/src/events/events.ts
+++ services/messages/src/events/events.ts
@@ -1,7 +1,4 @@
-import { DirectRepository } from "../repositories/direct/direct-repository";
-import { DirectRepositoryImpl } from "../repositories/direct/direct-repository-impl";
 import { RabbitMQ } from "../utils/rabbit-mq";
-import { ServerRepository } from "../repositories/server/server-repository";
 import { ServerRepositoryImpl } from "../repositories/server/server-repository-impl";
 /**
  * Service events
@@ -10,53 +7,52 @@ import { ServerRepositoryImpl } from "../repositories/server/server-repository-i
  * It is also responsible for updating the database.
  */
 export class ServiceEvents {
-	private static broker: RabbitMQ;
-	private static serverRepository: ServerRepositoryImpl = new ServerRepositoryImpl();
+  private static broker: RabbitMQ;
 
-	static async initialize() {
-		this.broker = RabbitMQ.getInstance();
-		await this.declareQueue();
-		await this.setupListeners();
-	}
+  static async initialize() {
+    this.broker = RabbitMQ.getInstance();
+    await this.declareQueue();
+    await this.setupListeners();
+  }
 
-	static async declareQueue() {
-		const channel = this.broker.getChannel();
+  static async declareQueue() {
+    const channel = this.broker.getChannel();
 
-		// Declare the exchange
-		await channel?.assertExchange("servers", "fanout", {
-			durable: true,
-		});
-	}
+    // Declare the exchange
+    await channel?.assertExchange("messages", "fanout", {
+      durable: true,
+    });
+  }
 
-	static async setupListeners() {
-		this.subscribeToExchange("servers", async (event, data) => {
-			switch (event) {
-				case "server.created":
-					await this.serverRepository.addServer(data.serverId, data.owner);
-			}
-		});
-	}
+  static async setupListeners() {
+    this.subscribeToExchange("messages", async (event, data) => {
+      switch (event) {
+        case "":
+          break;
+      }
+    });
+  }
 
-	private static async subscribeToExchange(
-		exchange: string,
-		callback: (event: string, data: any) => void
-	) {
-		const channel = this.broker.getChannel();
-		const queue = await channel?.assertQueue("", {
-			exclusive: true,
-		});
-		if (!queue) {
-			return;
-		}
-		await channel?.bindQueue(queue.queue, exchange, "");
-		channel?.consume(queue.queue, async (message) => {
-			if (!message) {
-				return;
-			}
+  private static async subscribeToExchange(
+    exchange: string,
+    callback: (event: string, data: any) => void
+  ) {
+    const channel = this.broker.getChannel();
+    const queue = await channel?.assertQueue("", {
+      exclusive: true,
+    });
+    if (!queue) {
+      return;
+    }
+    await channel?.bindQueue(queue.queue, exchange, "");
+    channel?.consume(queue.queue, async (message) => {
+      if (!message) {
+        return;
+      }
 
-			const content = message.content.toString();
-			const data = JSON.parse(content);
-			callback(message.fields.routingKey, data);
-		});
-	}
+      const content = message.content.toString();
+      const data = JSON.parse(content);
+      callback(message.fields.routingKey, data);
+    });
+  }
 }
