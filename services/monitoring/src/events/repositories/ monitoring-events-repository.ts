3f4cec805706f828fdda16c2abee592abbade51c
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-08-19 17:35:34 +0200

    fix: add alias in imports

--- services/monitoring/src/events/repositories/ monitoring-events-repository.ts
+++ services/monitoring/src/events/repositories/ monitoring-events-repository.ts
@@ -1,11 +1,9 @@
-import { RabbitMQ } from "@piperchat/commons";
 import { BasicEventsRepository } from "./basic-events-repository";
 
 /**
  * Entity events repository
  * It is responsible for publishing events to the message broker.
  */
-export class MessageEventsRepository extends BasicEventsRepository{
-	
-	// TODO: Add more events
+export class MessageEventsRepository extends BasicEventsRepository {
+  // TODO: Add more events
 }
