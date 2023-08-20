5c5018eb52aa0df738fce28a89cc921a27208550
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-08-20 17:32:39 +0200

    wip: implement service event in users service and refactor

--- services/commons/src/service-events-repository.ts
+++ services/commons/src/service-events-repository.ts
@@ -1,32 +1,35 @@
-import { BasicEventsRepository } from "./basic-events-repository";
+import { BasicEventsRepository } from './basic-events-repository'
 
-export class ServiceEventsRepository extends BasicEventsRepository{
-
-    async publishServiceStatusOnline(serviceName: string): Promise<void> {
-        const channel = this.getChannel();
-        if (channel) {
-            channel.publish(
-                'services',
-                'new.service.status',
-                Buffer.from(JSON.stringify({
-                    service: serviceName,
-                    status: 'online'
-                }))
-            )
-        }
+export class ServiceEventsRepository extends BasicEventsRepository {
+  async publishServiceStatusOnline(serviceName: string): Promise<void> {
+    const channel = this.getChannel()
+    if (channel) {
+      channel.publish(
+        'services',
+        'new.service.status',
+        Buffer.from(
+          JSON.stringify({
+            service: serviceName,
+            status: 'online',
+          })
+        )
+      )
     }
+  }
 
-    async publishServiceStatusOffline(serviceName: string): Promise<void> {
-        const channel = this.getChannel();
-        if (channel) {
-            channel.publish(
-                'services',
-                'new.service.status',
-                Buffer.from(JSON.stringify({
-                    service: serviceName,
-                    status: 'offline'
-                }))
-            )
-        }
+  async publishServiceStatusOffline(serviceName: string): Promise<void> {
+    const channel = this.getChannel()
+    if (channel) {
+      channel.publish(
+        'services',
+        'new.service.status',
+        Buffer.from(
+          JSON.stringify({
+            service: serviceName,
+            status: 'offline',
+          })
+        )
+      )
     }
+  }
 }
