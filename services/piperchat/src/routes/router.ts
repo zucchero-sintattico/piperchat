4dc4b95f7f4c80843e849d0284e4bdf00c6452ba
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-08-25 17:53:09 +0200

    feat: import healtcheck router in piperchat service

--- services/piperchat/src/routes/router.ts
+++ services/piperchat/src/routes/router.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { serverRouter } from './routers/server-router'
 import { channelRouter } from './routers/channel-router'
+import { HealthCheckRouter } from '@piperchat/commons'
 import { JWTAuthenticationMiddleware } from '@piperchat/commons'
 
 const serviceRouter = Router()
+const healthCheckRouter = Router()
 serviceRouter.use(JWTAuthenticationMiddleware)
 
+healthCheckRouter.use('/healthcheck', HealthCheckRouter)
 // Register all routers
 serviceRouter.use('/servers/:serverId/channels', channelRouter)
 serviceRouter.use('/servers', serverRouter)
