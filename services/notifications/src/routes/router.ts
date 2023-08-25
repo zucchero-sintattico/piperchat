7b6a1f83c9aaa3c778a5c866d1f5b1161f2482f7
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-08-25 18:06:07 +0200

    feat: add healthcheck router in notifications and webrtc services

--- services/notifications/src/routes/router.ts
+++ services/notifications/src/routes/router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { notificationRouter } from './routers/notification-router'
 import { userStatusRouter } from './routers/user-status-router'
+import { healthCheckRouter } from '@piperchat/commons'
 
 const serviceRouter = Router()
 
 serviceRouter.use('/notification', notificationRouter)
 serviceRouter.use('/users', userStatusRouter)
+serviceRouter.use('/healthcheck', healthCheckRouter)
 
 serviceRouter.get('/', (req, res) => {
   res.sendFile('client.html', { root: __dirname + '/../../' })
--- services/webrtc/src/routes/router.ts
+++ services/webrtc/src/routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { infraServiceMiddleware } from '@piperchat/commons'
 import { sessionRouter } from './routers/sessionRouter'
+import { healthCheckRouter } from '@piperchat/commons'
 
 export const serviceRouter = Router()
 serviceRouter.use(infraServiceMiddleware)
 
 serviceRouter.use('/session', sessionRouter)
+serviceRouter.use('/healthcheck', healthCheckRouter)
