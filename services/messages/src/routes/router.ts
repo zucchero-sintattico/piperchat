552902fbd8a29b901870ea880b5cb805b9e6bac9
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-08-26 11:18:42 +0200

    fix: add health router in messages

--- services/messages/src/routes/router.ts
+++ services/messages/src/routes/router.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
 import { directRouter } from './message-router'
 import { channelRouter } from './channels-router'
-import { healthCheckRouter } from '@piperchat/commons'
 
 const serviceRouter = Router()
 
 // Register all routers
 serviceRouter.use('/users', directRouter)
 serviceRouter.use('/servers', channelRouter)
-serviceRouter.use('/healthcheck', healthCheckRouter)
 
 export { serviceRouter }
