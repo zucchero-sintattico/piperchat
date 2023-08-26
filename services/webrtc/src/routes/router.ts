2f38a1a774409a9afd12b106f2e6734362ba24e1
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-08-26 11:23:11 +0200

    fix: add health router in webrtc

--- services/webrtc/src/routes/router.ts
+++ services/webrtc/src/routes/router.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express'
 import { infraServiceMiddleware } from '@piperchat/commons'
 import { sessionRouter } from './routers/sessionRouter'
-import { healthCheckRouter } from '@piperchat/commons'
 
 export const serviceRouter = Router()
 serviceRouter.use(infraServiceMiddleware)
 
 serviceRouter.use('/session', sessionRouter)
-serviceRouter.use('/healthcheck', healthCheckRouter)
