6a2aeb514bc8807c4b9b2f2443beb71455f94999
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-09-07 16:53:18 +0200

    fix(loginComponent): add right fields and implement register component

--- services/frontend/src/components/login-component/RegisterForm.vue
+++ services/frontend/src/components/login-component/RegisterForm.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 
 const username = ref('')
+const email = ref('')
 const password = ref('')
 const accept = ref(false)
 
@@ -27,16 +28,27 @@ function onReset() {
 
     <q-input
       filled
+      v-model="email"
+      label="Email"
+      hint="Insert your email"
+      lazy-rules
+      :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+    />
+
+    <q-input
+      filled
       type="password"
       v-model="password"
       label="Your password"
-      hint="Insert your password"
       lazy-rules
       :rules="[
         (val) => (val && val.length > 0) || 'Please type something',
         (val) => (val && val.length > 7) || 'Password must be at least 8 characters long'
       ]"
     />
+
+    <q-toggle v-model="accept" label="I accept the license and terms" />
+
     <div>
       <q-btn label="Submit" type="submit" color="primary" />
       <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
