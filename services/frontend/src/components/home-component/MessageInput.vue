21b4dfafebbf1670b76c31d9f31c9207f361463d
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-09-11 19:34:44 +0200

    wip(chat): add MessageInput component

---
+++ services/frontend/src/components/home-component/MessageInput.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const message = ref('')
+
+function sendMessage() {
+    console.log(message.value)
+    // TODO
+}
+</script>
+
+<template>
+    <div class="row justify-center foot">
+      <q-input
+        class="col-11 text"
+        outlined
+        placeholder="Type a message..."
+        v-model="message"
+        @keyup.enter="sendMessage"
+      ></q-input>
+      <q-btn class="col-1 btnn" color="primary" dense flat icon="send" @click="sendMessage" />
+    </div>
+</template>
+
+<style scoped>
+.foot {
+    position: sticky;
+    bottom:0;
+}
+
+.text {
+    background-color: white;
+}
+
+.btnn {
+    background-color: var(--primary);
+}
+
+
+</style>
