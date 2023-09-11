ba0f2250294e7a1da1e338706706a8aa8afa941b
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-09-11 19:35:41 +0200

    wip(message): init chat layout

--- services/frontend/src/components/home-component/ContentArea.vue
+++ services/frontend/src/components/home-component/ContentArea.vue
@@ -1,37 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MessageInput from '@/components/home-component/MessageInput.vue'
+</script>
 
 <template>
-  <div class="q-ma-md full-height chat">
-    <q-scroll-area visible class="full-height">
-      <div class="q-pa-md row justify-center">
-        <div v-for="n in 10" :key="n">
-          <q-chat-message
-            name="me"
-            avatar="https://cdn.quasar.dev/img/avatar1.jpg"
-            :text="['hey, how are you?']"
-            sent
-            bg-color="primary"
-            text-color="white"
-          />
-          <q-chat-message
-            name="Jane"
-            avatar="https://cdn.quasar.dev/img/avatar2.jpg"
-            :text="['doing fine, how r you?']"
-          />
-        </div>
-      </div>
-    </q-scroll-area>
-    <div class="row justify-center" style="bottom: sticky">
-      <q-input
-        style="width: 90%"
-        outlined
-        placeholder="Type a message..."
-        v-model="message"
-        @keyup.enter="sendMessage"
-      ></q-input>
-      <q-btn color="primary" round dense flat icon="send" @click="sendMessage" />
+  <!--Message-->
+  <div class="q-pa-md row justify-center">
+    <div v-for="n in 10" :key="n" style="width: 90%; max-width: 100%">
+      <q-chat-message
+        name="me"
+        avatar="https://cdn.quasar.dev/img/avatar1.jpg"
+        :text="['hey, how are you?']"
+        sent
+      />
+      <q-chat-message
+        name="Jane"
+        avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+        :text="['doing fine, how r you?']"
+      />
     </div>
   </div>
+
+  <MessageInput />
 </template>
 
 <style></style>
--- services/frontend/src/components/home-component/MessageInput.vue
+++ services/frontend/src/components/home-component/MessageInput.vue
@@ -1,41 +1,38 @@
 <script setup lang="ts">
-
 import { ref } from 'vue'
 
 const message = ref('')
 
 function sendMessage() {
-    console.log(message.value)
-    // TODO
+  console.log(message.value)
+  // TODO
 }
 </script>
 
 <template>
-    <div class="row justify-center foot">
-      <q-input
-        class="col-11 text"
-        outlined
-        placeholder="Type a message..."
-        v-model="message"
-        @keyup.enter="sendMessage"
-      ></q-input>
-      <q-btn class="col-1 btnn" color="primary" dense flat icon="send" @click="sendMessage" />
-    </div>
+  <div class="row justify-center foot">
+    <q-input
+      class="col-11 text"
+      outlined
+      placeholder="Type a message..."
+      v-model="message"
+      @keyup.enter="sendMessage"
+    ></q-input>
+    <q-btn class="col-1 btnn" color="primary" dense flat icon="send" @click="sendMessage" />
+  </div>
 </template>
 
 <style scoped>
 .foot {
-    position: sticky;
-    bottom:0;
+  position: sticky;
+  bottom: 0;
 }
 
 .text {
-    background-color: white;
+  background-color: white;
 }
 
 .btnn {
-    background-color: var(--primary);
+  background-color: var(--primary);
 }
-
-
 </style>
