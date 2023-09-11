fe3ff4c6fc53e32809d14334e0072703c31e96a1
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-09-11 16:58:04 +0200

    chore: pretty

--- services/frontend/src/components/home-component/ServerList.vue
+++ services/frontend/src/components/home-component/ServerList.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-    <q-scroll-area visible class="col-4 bg-dark">
-        <div class="column items-center">
-            <div v-for="n in 40" :key="n" class="col q-mt-md">
-                <q-avatar color="accent" text-color="dark">{{ n }}</q-avatar>
-            </div>
-        </div>
-    </q-scroll-area>
+  <q-scroll-area visible class="col-4 bg-dark">
+    <div class="column items-center">
+      <div v-for="n in 40" :key="n" class="col q-mt-md">
+        <q-avatar color="accent" text-color="dark">{{ n }}</q-avatar>
+      </div>
+    </div>
+  </q-scroll-area>
 </template>
--- services/users/src/events-configuration.ts
+++ services/users/src/events-configuration.ts
@@ -1,5 +1,4 @@
 import { EventsConfiguration } from '@commons/events/events-configuration'
-import { UserCreatedMessage, UserDeletedMessage } from '@messages-api/users'
 
 export class UserServiceEventsConfiguration extends EventsConfiguration {
   constructor() {
