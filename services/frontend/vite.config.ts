b19be42ea671adce0bb20b77ebaf490e04d7a050
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-09-11 15:33:24 +0200

    Almost made frontend work

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@api': fileURLToPath(new URL('../api/src', import.meta.url))
     }
   },
   cacheDir: '../../node_modules/.vite',
