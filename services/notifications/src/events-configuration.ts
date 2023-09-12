3d7441c3f6628db47ef9efa4b63c2345753c687c
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-09-12 11:05:51 +0200

    feat: added servers event listening on notification to update model and have participants reference

--- services/notifications/src/events-configuration.ts
+++ services/notifications/src/events-configuration.ts
@@ -11,14 +11,35 @@ import {
   FriendRequestSentMessage,
   FriendRequestAcceptedMessage,
 } from '@messages-api/friends'
+import {
+  ServerCreated,
+  ServerDeleted,
+  UserJoinedServer,
+  UserLeftServer,
+  UserKickedFromServer,
+} from '@messages-api/servers'
 import { UserOnlineMessage, UserOfflineMessage } from '@messages-api/users'
+import { Servers } from './models/server'
 
 export class NotificationsServiceEventsConfiguration extends EventsConfiguration {
   constructor() {
     super()
+    this.listenToServersUpdates()
 
     this.on(NewMessageOnChannel, async (event: NewMessageOnChannel) => {
-      // TODO: Send to channel members
+      const server = await Servers.findOne({ _id: event.serverId })
+      const participants = server?.participants.filter(
+        (participant) => participant !== event.sender
+      )
+      participants?.forEach((participant) => {
+        notifiableUsers.sendIfPresent(
+          participant,
+          new NewMessageNotification({
+            from: event.sender,
+            content: event.message,
+          })
+        )
+      })
     })
 
     this.on(NewMessageOnDirect, async (event: NewMessageOnDirect) => {
@@ -55,6 +76,41 @@ export class NotificationsServiceEventsConfiguration extends EventsConfiguration
 
     this.on(UserOfflineMessage, async (event: UserOfflineMessage) => {
       // TODO: Send to friends
+    })
+  }
+
+  listenToServersUpdates() {
+    this.on(ServerCreated, async (event: ServerCreated) => {
+      await Servers.create({
+        _id: event.id,
+        owner: event.owner,
+        participants: [event.owner],
+      })
+    })
+
+    this.on(ServerDeleted, async (event: ServerDeleted) => {
+      await Servers.deleteOne({ _id: event.id })
+    })
+
+    this.on(UserJoinedServer, async (event: UserJoinedServer) => {
+      await Servers.updateOne(
+        { _id: event.serverId },
+        { $push: { participants: event.username } }
+      )
+    })
+
+    this.on(UserLeftServer, async (event: UserLeftServer) => {
+      await Servers.updateOne(
+        { _id: event.serverId },
+        { $pull: { participants: event.username } }
+      )
+    })
+
+    this.on(UserKickedFromServer, async (event: UserKickedFromServer) => {
+      await Servers.updateOne(
+        { _id: event.serverId },
+        { $pull: { participants: event.username } }
+      )
     })
   }
 }
