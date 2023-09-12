31c5b9ea59c952617dae19f695c5b9311b699b2e
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2023-09-12 17:37:30 +0200

    reactive switch betwen DirectList and ChannelList

--- services/frontend/src/components/home-component/ServerList.vue
+++ services/frontend/src/components/home-component/ServerList.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const event = defineEmits<{
+  (e: 'OpenChannels'): () => void
+  (e: 'OpenDirects'): () => void
+}>()
+
+function OpenChannels() {
+  event('OpenChannels')
+}
+
+function OpenDirects() {
+  event('OpenDirects')
+}
+</script>
 
 <template>
   <div class="column">
     <q-item>
-      <q-btn size="20px" color="primary" round icon="chat" />
+      <q-btn size="20px" color="primary" round icon="chat" @click="OpenDirects" />
     </q-item>
 
     <q-separator color="accent" inset />
@@ -11,7 +24,14 @@
     <q-scroll-area visible class="col bg-dark">
       <div v-for="n in 40" :key="n" class="col">
         <q-item>
-          <q-btn size="20px" color="accent" round :label="n" text-color="primary" />
+          <q-btn
+            size="20px"
+            color="accent"
+            round
+            :label="n"
+            text-color="primary"
+            @click="OpenChannels"
+          />
         </q-item>
       </div>
     </q-scroll-area>
