3f0170d74470c7e1e874bf358a0e8d94b963bd88
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-09-12 10:40:01 +0200

    wip: implementing event redirect by notifications service

--- services/api/src/notifications/messages.ts
+++ services/api/src/notifications/messages.ts
@@ -1,9 +1,25 @@
-export class NewMessage {
+export class NewMessageNotification {
   type = 'new-message' as const
   from: string
   content: string
   constructor(data: { from: string; content: string }) {
     this.from = data.from
     this.content = data.content
+  }
+}
+
+export class FriendRequestNotification {
+  type = 'friend-request' as const
+  from: string
+  constructor(data: { from: string }) {
+    this.from = data.from
+  }
+}
+
+export class FriendRequestAcceptedNotification {
+  type = 'friend-request-accepted' as const
+  from: string
+  constructor(data: { from: string }) {
+    this.from = data.from
   }
 }
--- services/notifications/src/events-configuration.ts
+++ services/notifications/src/events-configuration.ts
@@ -1,7 +1,60 @@
 import { EventsConfiguration } from '@commons/events/events-configuration'
+import { NewMessageOnChannel } from '@messages-api/channels'
+import { NewMessageOnDirect } from '@messages-api/directs'
+import { notifiableUsers } from './models/notification-model'
+import {
+  FriendRequestAcceptedNotification,
+  FriendRequestNotification,
+  NewMessageNotification,
+} from '@api/notifications/messages'
+import {
+  FriendRequestSentMessage,
+  FriendRequestAcceptedMessage,
+} from '@messages-api/friends'
+import { UserOnlineMessage, UserOfflineMessage } from '@messages-api/users'
 
 export class NotificationsServiceEventsConfiguration extends EventsConfiguration {
   constructor() {
     super()
+
+    this.on(NewMessageOnChannel, async (event: NewMessageOnChannel) => {
+      // TODO: Send to channel members
+    })
+
+    this.on(NewMessageOnDirect, async (event: NewMessageOnDirect) => {
+      notifiableUsers.sendIfPresent(
+        event.receiver,
+        new NewMessageNotification({
+          from: event.sender,
+          content: event.message,
+        })
+      )
+    })
+
+    this.on(FriendRequestSentMessage, async (event: FriendRequestSentMessage) => {
+      notifiableUsers.sendIfPresent(
+        event.to,
+        new FriendRequestNotification({
+          from: event.from,
+        })
+      )
+    })
+
+    this.on(FriendRequestAcceptedMessage, async (event: FriendRequestAcceptedMessage) => {
+      notifiableUsers.sendIfPresent(
+        event.to,
+        new FriendRequestAcceptedNotification({
+          from: event.from,
+        })
+      )
+    })
+
+    this.on(UserOnlineMessage, async (event: UserOnlineMessage) => {
+      // TODO: Send to friends
+    })
+
+    this.on(UserOfflineMessage, async (event: UserOfflineMessage) => {
+      // TODO: Send to friends
+    })
   }
 }
