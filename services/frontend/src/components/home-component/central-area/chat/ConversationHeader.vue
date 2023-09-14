152a2485c82798d86f6a19d83dfeacfd7957312a
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-09-14 15:44:12 +0200

    fix(header): change title switch system

--- services/frontend/src/components/home-component/central-area/chat/ConversationHeader.vue
+++ services/frontend/src/components/home-component/central-area/chat/ConversationHeader.vue
@@ -17,16 +17,22 @@ watch(
   }
 )
 
-const channelName = computed(() => {
-  const server = serverStore.servers.filter((server) => server._id == userStore.selectedServerId)[0]
-  if (server) {
-    const channel = server.channels.filter((channel) => channel._id == userStore.selectedChannel[1])
-    if (channel.length > 0) {
-      return channel[0].name
+watch(
+  () => userStore.selectedChannel,
+  (newVal) => {
+    if (newVal[1] !== '') {
+      const server = serverStore.servers.filter((server) => server._id == newVal[0])[0]
+
+      if (server) {
+        const channel = server.channels.filter((channel) => channel._id == newVal[1])
+
+        if (channel.length > 0) {
+          title.value = channel[0].name
+        }
+      }
     }
   }
-  return ''
-})
+)
 </script>
 <template>
   <q-header>
@@ -34,10 +40,7 @@ const channelName = computed(() => {
       <!-- <q-avatar>
         <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
       </q-avatar> -->
-      <q-toolbar-title v-if="userStore.inContentArea == ContentArea.Channel">
-        {{ channelName }}
-      </q-toolbar-title>
-      <q-toolbar-title v-if="userStore.inContentArea == ContentArea.Direct">
+      <q-toolbar-title>
         {{ title }}
       </q-toolbar-title>
     </q-toolbar>
