c87b553814db7cb9e6ce67382a9d2d78628a534a
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2023-09-14 11:38:57 +0200

    default user img

--- services/frontend/src/components/home-component/left-bar/ChannelsList.vue
+++ services/frontend/src/components/home-component/left-bar/ChannelsList.vue
@@ -24,57 +24,31 @@ const selectedServer = computed(() => {
 
         <!-- start Create new server -->
         <div class="q-ma-md" style="text-align: center">
-          <q-btn
-            color="primary"
-            label="Create channel"
-            icon="add"
-            style="justify-content: space-between"
-            @click="isNewChannelFormActive = true"
-          />
+          <q-btn color="primary" label="Create channel" icon="add" style="justify-content: space-between"
+            @click="isNewChannelFormActive = true" />
         </div>
-        <q-dialog
-          v-model="isNewChannelFormActive"
-          persistent
-          transition-show="scale"
-          transition-hide="scale"
-        >
+        <q-dialog v-model="isNewChannelFormActive" persistent transition-show="scale" transition-hide="scale">
           <NewChannelForm @close="isNewChannelFormActive = false" />
         </q-dialog>
         <!-- end Create new server -->
 
         <!-- start Message channels -->
-        <q-list
-          bordered
-          separator
-          class="text-white text-h5"
-          v-for="channel in selectedServer?.channels?.filter(
-            (c) => c.channelType == CreateChannelApi.ChannelType.Messages
-          )"
-          :key="channel._id"
-        >
+        <q-list bordered separator class="text-white text-h5" v-for="channel in selectedServer?.channels?.filter(
+          (c) => c.channelType == CreateChannelApi.ChannelType.Messages
+        )" :key="channel._id">
           <HorizontalChannel :name="channel.name" icon="chat" clickable />
         </q-list>
         <!-- end Message channels -->
 
         <!-- start Multimedia channels -->
-        <q-list
-          bordered
-          separator
-          class="text-white text-h5"
-          v-for="channel in selectedServer?.channels?.filter(
-            (c) => c.channelType == CreateChannelApi.ChannelType.Multimedia
-          )"
-          :key="channel._id"
-        >
-          <HorizontalChannel
-            :name="channel.name"
-            icon="volume_up"
-            clickable
-            @click="userStore.setActiveChannel(channel._id)"
-          />
+        <q-list bordered separator class="text-white text-h5" v-for="channel in selectedServer?.channels?.filter(
+          (c) => c.channelType == CreateChannelApi.ChannelType.Multimedia
+        )" :key="channel._id">
+          <HorizontalChannel :name="channel.name" icon="volume_up" clickable
+            @click="userStore.setActiveChannel(channel._id)" />
 
           <q-list dense v-for="j in 3" :key="j">
-            <HorizontalUser name="User" photo="https://cdn.quasar.dev/img/avatar3.jpg" />
+            <HorizontalUser name="User" photo='' />
           </q-list>
         </q-list>
         <!-- end Multimedia channels -->
