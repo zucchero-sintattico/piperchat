fe5274991c3d7081be87d474a7130c7f316f8f0f
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2023-09-15 15:04:14 +0200

    ellipsis on directs list

--- services/frontend/src/components/home-component/left-bar/DirectsList.vue
+++ services/frontend/src/components/home-component/left-bar/DirectsList.vue
@@ -5,14 +5,18 @@ import { useUserStore } from '@/stores/user'
 const userStore = useUserStore()
 </script>
 <template>
-  <q-scroll-area visible class="col bg-secondary">
+  <q-scroll-area
+    class="col bg-secondary fit"
+    content-style="position: relative !important"
+    content-active-style="position: relative !important"
+  >
     <div class="column">
-      <div class="col">
+      <div class="col fit">
         <q-list bordered separator class="text-white text-h5">
           <HorizontalChannel name="Directs" icon="chat" />
-          <q-list bordered separator class="text-white text-h5" v-for="user in 10" :key="user">
+          <q-list bordered separator class="text-white text-h5" v-for="user in 100" :key="user">
             <HorizontalUser
-              :name="user"
+              :name="user + 'wfewrfewrfewrfwrfvrf'"
               photo=""
               icon="chat"
               @click="userStore.selectedDirect = String(user)"
--- services/frontend/src/components/home-component/left-bar/horizontal-component/HorizontalChannel.vue
+++ services/frontend/src/components/home-component/left-bar/horizontal-component/HorizontalChannel.vue
@@ -5,6 +5,8 @@ const props = defineProps(['name', 'icon', 'clickable'])
 <template>
   <q-item style="align-items: center" :clickable="props.clickable">
     <q-icon :name="props.icon" class="q-pr-md" />
-    <q-item-section class="ellipsis">{{ props.name }}</q-item-section>
+    <q-item-section class="ellipsis">
+      {{ props.name }}
+    </q-item-section>
   </q-item>
 </template>
