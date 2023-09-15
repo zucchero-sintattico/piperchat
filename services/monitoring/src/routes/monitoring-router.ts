5d322598437b4370f2725215a54f09ccb9550a23
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-09-15 16:35:19 +0200

    feat: completed backend check

--- services/monitoring/src/routes/monitoring-router.ts
+++ services/monitoring/src/routes/monitoring-router.ts
@@ -1,28 +1,21 @@
 import { MonitoringController } from '@/controllers/monitoring-controller'
 import { MonitoringControllerImpl } from '@/controllers/monitoring-controller-impl'
-
-import { InternalServerError } from '@api/errors'
 import { GetServicesStatusApi } from '@api/monitoring/status'
-import { LoginApi } from '@api/users/auth'
-import { ApiRouter } from '@commons/router'
-
-const monitoringRouter: ApiRouter = new ApiRouter()
+import { Route } from '@commons/router'
+import { Router } from 'express'
 
 const monitoringController: MonitoringController = new MonitoringControllerImpl()
 
-monitoringRouter.get<LoginApi.Request.Params, LoginApi.Request.Body, LoginApi.Response>(
-  '/',
-  LoginApi.Request.Schema,
-  async (req, res) => {
-    try {
-      const monitoring = await monitoringController.getServiceStatus()
-      const response = new GetServicesStatusApi.Responses.Success(monitoring)
-      response.send(res)
-    } catch (e) {
-      const response = new InternalServerError(e)
-      response.send(res)
-    }
-  }
-)
+const MonitoringApiRoute = new Route<GetServicesStatusApi.Response>({
+  method: 'get',
+  path: '/',
+  schema: GetServicesStatusApi.Request.Schema,
+  handler: async (req, res) => {
+    const monitoring = await monitoringController.getServiceStatus()
+    const response = new GetServicesStatusApi.Responses.Success(monitoring)
+    res.sendResponse(response)
+  },
+})
 
-export { monitoringRouter }
+export const monitoringRouter = Router()
+MonitoringApiRoute.attachToRouter(monitoringRouter)
