06e18dee99e3ed53d128572352304a14286d3f05
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2023-09-17 19:29:26 +0200

    dynamic color

--- services/frontend/src/components/monitoring-component/StatusCard.vue
+++ services/frontend/src/components/monitoring-component/StatusCard.vue
@@ -5,18 +5,27 @@ import { onMounted } from 'vue'
 const monitoringStore = useMonitoringStore()
 
 onMounted(() => {
-    monitoringStore.getServicesStatus()
+  monitoringStore.getServicesStatus()
 })
 </script>
 <template>
-    <q-item v-for="service in monitoringStore.serviceStatus" :key="service">
-        <q-card bordered class="bg-red-3">
-            <q-card-section>
-                <div class="text-h6">{{ service.service }}</div>
-            </q-card-section>
+  <q-item v-for="service in monitoringStore.serviceStatus" :key="service">
+    <q-card bordered :class="service.status">
+      <q-card-section>
+        <div class="text-h6">{{ service.service }}</div>
+      </q-card-section>
 
-            <q-card-section class="q-pt-none"> {{ service.status }} </q-card-section>
-            <q-card-section class="q-pt-none"> {{ service.timestamp }} </q-card-section>
-        </q-card>
-    </q-item>
+      <q-card-section class="q-pt-none"> {{ service.status }} </q-card-section>
+      <q-card-section class="q-pt-none"> {{ service.timestamp }} </q-card-section>
+    </q-card>
+  </q-item>
 </template>
+<style scoped lang="scss">
+.offline {
+  background-color: $red-3;
+}
+
+.online {
+  background-color: $green-3;
+}
+</style>
