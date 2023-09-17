c656dad6e688bee6e6dec77565a82bfe817362f8
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2023-09-17 19:14:51 +0200

    monitoring card

--- services/frontend/src/components/monitoring-component/StatusCard.vue
+++ services/frontend/src/components/monitoring-component/StatusCard.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import { useMonitoringStore } from '@/stores/monitoring'
-import { computed } from 'vue'
 import { onMounted } from 'vue'
 
 const monitoringStore = useMonitoringStore()
 
 onMounted(() => {
-  monitoringStore.getServicesStatus()
+    monitoringStore.getServicesStatus()
 })
 </script>
 <template>
-  <q-item v-for="service in monitoringStore.serviceStatus" :key="service">
-    <q-card bordered class="bg-red-3">
-      <q-card-section>
-        <div class="text-h6">{{ service }}</div>
-      </q-card-section>
+    <q-item v-for="service in monitoringStore.serviceStatus" :key="service">
+        <q-card bordered class="bg-red-3">
+            <q-card-section>
+                <div class="text-h6">{{ service.service }}</div>
+            </q-card-section>
 
-      <q-card-section class="q-pt-none"> Service Name </q-card-section>
-    </q-card>
-  </q-item>
+            <q-card-section class="q-pt-none"> {{ service.status }} </q-card-section>
+            <q-card-section class="q-pt-none"> {{ service.timestamp }} </q-card-section>
+        </q-card>
+    </q-item>
 </template>
