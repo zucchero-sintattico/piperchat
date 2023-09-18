4c6da13f3e51ae2357b2b90327d853a713468f40
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2023-09-18 11:12:43 +0200

    status refresh at update

--- services/frontend/src/stores/monitoring.ts
+++ services/frontend/src/stores/monitoring.ts
@@ -4,27 +4,23 @@ import type { MonitoringController } from '@/controllers/monitoring/monitoring-c
 import { MonitoringControllerImpl } from '@/controllers/monitoring/monitoring-controller'
 import { GetServicesStatusApi } from '@api/monitoring/status'
 
-export const useMonitoringStore = defineStore(
-  'monitoring',
-  () => {
-    const serviceStatus = ref()
+export const useMonitoringStore = defineStore('monitoring', () => {
+  const monitoringController: MonitoringController = new MonitoringControllerImpl()
+  const serviceStatus = ref()
 
-    async function getServicesStatus() {
-      const monitoringController: MonitoringController = new MonitoringControllerImpl()
-      const response =
-        (await monitoringController.getServicesStatus()) as GetServicesStatusApi.Responses.Success
-      if (response.statusCode === 200) {
-        const typed = response as GetServicesStatusApi.Responses.Success
-        serviceStatus.value = typed.services
-      } else {
-        console.log(response)
-      }
+  async function getServicesStatus() {
+    const response =
+      (await monitoringController.getServicesStatus()) as GetServicesStatusApi.Responses.Success
+    if (response.statusCode === 200) {
+      const typed = response as GetServicesStatusApi.Responses.Success
+      serviceStatus.value = typed.services
+    } else {
+      console.log(response)
     }
+  }
 
-    return {
-      serviceStatus,
-      getServicesStatus
-    }
-  },
-  { persist: true }
-)
+  return {
+    serviceStatus,
+    getServicesStatus
+  }
+})
