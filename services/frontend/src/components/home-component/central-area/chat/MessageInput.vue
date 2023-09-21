1c5ebe2f8a66056bf69375472f4406096c75df3e
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2023-09-21 09:31:22 +0200

    message input overlap fixed

--- services/frontend/src/components/home-component/central-area/chat/MessageInput.vue
+++ services/frontend/src/components/home-component/central-area/chat/MessageInput.vue
@@ -68,7 +68,7 @@ function deleteMessage() {
 </script>
 
 <template>
-  <div class="foot blurred" v-if="shown">
+  <q-footer>
     <q-input padding filled v-model="message" label="Write..." @keydown.enter.prevent="sendMessage">
       <template v-slot:append>
         <q-icon v-if="message !== ''" name="close" @click="deleteMessage" class="cursor-pointer" />
@@ -77,11 +77,11 @@ function deleteMessage() {
         <q-btn round dense flat icon="send" color="primary" @click="sendMessage" id="send" />
       </template>
     </q-input>
-  </div>
+  </q-footer>
 </template>
 
-<style scoped lang="sass">
-.foot
-  position: sticky
-  bottom: 0
+<style scoped lang="scss">
+footer {
+  background-color: transparent !important;
+}
 </style>
