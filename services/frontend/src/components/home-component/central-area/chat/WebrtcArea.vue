cf1dd3dc50a98e6d13536c0c840636e47ee4cb10
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-10-21 11:58:33 +0200

    chore: added stream loading when enter webrtc and stream remove when leaving

--- services/frontend/src/components/home-component/central-area/chat/WebrtcArea.vue
+++ services/frontend/src/components/home-component/central-area/chat/WebrtcArea.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
+import {
+  SessionControllerImpl,
+  type SessionController
+} from '@/controllers/session/session-controller'
 import { useUserStore, ContentArea } from '@/stores/user'
-import { computed } from 'vue'
+import { computed, onMounted, onUnmounted, watch } from 'vue'
 const userStore = useUserStore()
 const toShow = computed(() => userStore.inContentArea == ContentArea.Multimedia)
+
+const jwtToken = localStorage.getItem('jwt')
+const sessionController: SessionController = new SessionControllerImpl(jwtToken || '')
+
+let myStream: MediaStream | null = null
+
+watch(toShow, async (inWebrtc) => {
+  if (inWebrtc) {
+    console.log('getting stream')
+    myStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    })
+  } else {
+    console.log('closing stream')
+    myStream?.getTracks().forEach((track) => track.stop())
+    myStream = null
+  }
+})
 </script>
 
 <template>
