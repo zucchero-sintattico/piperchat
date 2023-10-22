d57c44d880239d73336d8e42bd654db251a0335f
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-10-22 15:00:49 +0200

    feat(escKeypress): implement esc keypress functionality

--- services/frontend/src/views/HomeView.vue
+++ services/frontend/src/views/HomeView.vue
@@ -32,62 +32,64 @@ onMounted(() => {
 </script>
 
 <template>
-  <q-layout view="hHh Lpr lff" class="bg-accent">
-    <q-header elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+  <div @keydown.esc="userStore.inContentArea = ContentArea.empty">
+    <q-layout view="hHh Lpr lff" class="bg-accent">
+      <q-header elevated class="bg-primary text-white" height-hint="98">
+        <q-toolbar>
+          <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title align="center" class="title">
-          <div class="avatar-and-title">
-            <q-avatar>
-              <img src="src/assets/piperchat-logo.jpg" />
-            </q-avatar>
-            <h1>Piperchat</h1>
-          </div>
-        </q-toolbar-title>
+          <q-toolbar-title align="center" class="title">
+            <div class="avatar-and-title">
+              <q-avatar>
+                <img src="src/assets/piperchat-logo.jpg" />
+              </q-avatar>
+              <h1>Piperchat</h1>
+            </div>
+          </q-toolbar-title>
 
-        <!--Avatar image that toggle dropdown menu with logout and settings option-->
-        <q-avatar>
-          <!-- add hover effect to image -->
-          <img :src="userStore.photo" class="cursor-pointer" />
-          <q-menu align="right">
-            <q-list>
-              <q-item clickable v-close-popup @click="isSettingsFormActive = true">
-                <!--Add settings icon on left and 'Settings' text on right-->
-                <q-item-section avatar>
-                  <q-icon name="settings" />
-                </q-item-section>
-                <q-item-section> Settings </q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup @click="logout">
-                <q-item-section avatar>
-                  <q-icon name="logout" />
-                </q-item-section>
-                <q-item-section> Logout </q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </q-avatar>
-      </q-toolbar>
-      <q-dialog
-        v-model="isSettingsFormActive"
-        persistent
-        transition-show="scale"
-        transition-hide="scale"
-      >
-        <SettingsForm @close="isSettingsFormActive = false" />
-      </q-dialog>
-    </q-header>
+          <!--Avatar image that toggle dropdown menu with logout and settings option-->
+          <q-avatar>
+            <!-- add hover effect to image -->
+            <img :src="userStore.photo" class="cursor-pointer" />
+            <q-menu align="right">
+              <q-list>
+                <q-item clickable v-close-popup @click="isSettingsFormActive = true">
+                  <!--Add settings icon on left and 'Settings' text on right-->
+                  <q-item-section avatar>
+                    <q-icon name="settings" />
+                  </q-item-section>
+                  <q-item-section> Settings </q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup @click="logout">
+                  <q-item-section avatar>
+                    <q-icon name="logout" />
+                  </q-item-section>
+                  <q-item-section> Logout </q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-avatar>
+        </q-toolbar>
+        <q-dialog
+          v-model="isSettingsFormActive"
+          persistent
+          transition-show="scale"
+          transition-hide="scale"
+        >
+          <SettingsForm @close="isSettingsFormActive = false" />
+        </q-dialog>
+      </q-header>
 
-    <!-- pass leftDrawopen as props -->
-    <LeftBar show-if-above v-model="leftDrawerOpen" side="left" bordered />
+      <!-- pass leftDrawopen as props -->
+      <LeftBar show-if-above v-model="leftDrawerOpen" side="left" bordered />
 
-    <!--Use the rest part of the page to show ContentArea component-->
-    <q-page-container class="full-height">
-      <ContentArea />
-      <!-- <router-view /> -->
-    </q-page-container>
-  </q-layout>
+      <!--Use the rest part of the page to show ContentArea component-->
+      <q-page-container class="full-height">
+        <ContentArea />
+        <!-- <router-view /> -->
+      </q-page-container>
+    </q-layout>
+  </div>
 </template>
 
 <style>
